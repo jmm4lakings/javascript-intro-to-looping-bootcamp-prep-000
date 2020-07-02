49ac0776c37142764c943294fd7a95a58233d6b3
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-02 23:34:48 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,20 +1,32 @@
 function forLoop(array) {
-for (let i = 0; i < 25; i++) {
-  if (i === 1) {array.push("I am 1 strange loop.")}
-  else {array.push("I am ${i} strange loops.")}
-}return array;
+	for (let i = 0; i < 25; i++) {
+		if (i === 1) {
+			array.push('I am 1 strange loop.');
+		} else {
+			array.push(`I am ${i} strange loops.`);
+		}
+	}
+
+	return array;
 }
 
 function whileLoop(n) {
-  while (n > 0) {
-    console.log(--n)}
-  return "done"}
+	while (n > 0) {
+		console.log(--n);
+	}
 
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
+	return 'done';
+}
 
-function doWhileLoop(array) {
-  do {array.pop()}
-  while (array.length > 0 && maybeTrue());
-  return array}
+function doWhileLoop(num) {
+	var i = 0;
+
+	function incrementVariable() {
+		i = i + 1;
+    return i;
+	}
+
+  do {
+    console.log("I run once regardless.");
+  } while (incrementVariable() < num);
+} 
